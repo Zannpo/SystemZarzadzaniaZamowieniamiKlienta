9884076a4a36b6ce1bf3bafb61f56c162007e79c
Żan <zanpo00@gmail.com>
2021-11-11 00:43:52 +0100

    more cleaning

--- SystemZatzadzaniaZamowieniamiKlijenta_RESTAURACJA/Properties/Resources.Designer.cs
+++ SystemZatzadzaniaZamowieniamiKlijenta_RESTAURACJA/Properties/Resources.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace SystemZatzadzaniaZamowieniamiKlijenta_RESTAURACJA.Properties {
+namespace SystemZarzadzaniaZamowieniamiKlienta_RESTAURACJA.Properties {
     using System;
     
     
@@ -39,7 +39,7 @@ namespace SystemZatzadzaniaZamowieniamiKlijenta_RESTAURACJA.Properties {
         internal static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("SystemZatzadzaniaZamowieniamiKlijenta_RESTAURACJA.Properties.Resources", typeof(Resources).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("SystemZarzadzaniaZamowieniamiKlienta_RESTAURACJA.Properties.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
